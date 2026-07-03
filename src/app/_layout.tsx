03395960c30b29184136f025cdadcc6fbda2fa74
Sanskruti Chopade <sanskruti@example.com>
2026-07-03 14:03:59 +0530

    Add tab navigation and meal management screens

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,5 @@
- import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
- import { useColorScheme } from 'react-native';
-
+  
+import React from 'react';
 // import { AnimatedSplashOverlay } from '@/components/animated-icon';
 // import AppTabs from '@/components/app-tabs';
 
@@ -26,21 +25,12 @@
 //     />
 //   );
 // }
-
-import { colors } from '@/styles/global';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.header },
-        headerTintColor: '#fff',
-      }}
-    >
-      <Stack.Screen name='index' options={{ headerShown: false }} />
-      <Stack.Screen options={{ title: 'Meals' }} name='meals' />
-      <Stack.Screen options={{ title: 'Add Meal' }} name='add-meal' />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='(tabs)' />
     </Stack>
   );
 }
